81f7daaa27c3890118b31a88bc3f8f2868f039e5
Ishit Beswal <ib370@snu.edu.in>
2020-01-23 19:04:30 +0530

    h

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,5 +1,6 @@
-import React,{Component} from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/Colors"
 
 // export default function ContactScreen() {
 //   return (
@@ -13,136 +14,137 @@ import { ScrollView, StyleSheet, Text } from 'react-native';
 //   );
 // }
 
-
 export default class ContactScreen extends Component {
+  people = [
+    {
+      name: "Balaji Vunnava",
+      position: "ChairPerson",
+      phone: "7042049395",
+      email: "bv808@snu.edu.in"
+    },
+    {
+      name: "Rahul Goyal",
+      position: "Administrator",
+      phone: "9821400910",
+      email: "rg294@snu.edu.in"
+    },
+    {
+      name: "Mohnish Jagwani",
+      position: "Co-Chairperson",
+      phone: "8017035960",
+      email: "mj474@snu.edu.in"
+    },
+    {
+      name: "Anirudh Kaushik ",
+      position: "Technical Director",
+      phone: "9884983595",
+      email: "ak669@snu.edu.in"
+    },
+    {
+      name: "Madhur Gupta",
+      position: "Head of Finance",
+      phone: "9818245177",
+      email: "mg786@snu.edu.in"
+    },
+    {
+      name: "Mallika Gupta",
+      position: "Head of Public Relations",
+      phone: "9149392440",
+      email: "mg958@snu.edu.in"
+    },
+    {
+      name: "Sarath Chandra Mudigonda ",
+      position: "Head of Finance",
+      phone: "9884491080 ",
+      email: "sm261@snu.edu.in "
+    },
+    {
+      name: "Keval Sushruth",
+      position: "Sports Director",
+      phone: "8826243936",
+      email: "sk405@snu.edu.in"
+    },
+    {
+      name: "Kaustuv Acharyya ",
+      position: "Head of Public Relations",
+      phone: "9643427888",
+      email: "ka117@snu.edu.in"
+    },
+    {
+      name: "Abhinav Keshri",
+      position: "Cultural Director",
+      phone: "9835534434",
+      email: "ak825@snu.edu.in"
+    },
+    {
+      name: "Abhirami Reddy",
+      position: "Sports Director",
+      phone: "9515502260",
+      email: "ar197@snu.edu.in "
+    }
+  ];
 
-	
-  people= [
-     {
-       name: "Balaji Vunnava",
-       position: "ChairPerson",
-       phone: "7042049395",
-       email: "bv808@snu.edu.in"
-     },
-     {
-       name: "Rahul Goyal",
-       position: "Administrator",
-       phone: "9821400910",
-       email: "rg294@snu.edu.in"
-     },
-     {
-       name: "Mohnish Jagwani",
-       position: "Co-Chairperson",
-       phone: "8017035960",
-       email: "mj474@snu.edu.in"
-     },
-     {
-       name: "Anirudh Kaushik ",
-       position: "Technical Director",
-       phone: "9884983595",
-       email: "ak669@snu.edu.in"
-     },
-     {
-       name: "Madhur Gupta",
-       position: "Head of Finance",
-       phone: "9818245177",
-       email: "mg786@snu.edu.in"
-     },
-     {
-       name: "Mallika Gupta",
-       position: "Head of Public Relations",
-       phone: "9149392440",
-       email: "mg958@snu.edu.in"
-     },
-     {
-       name: "Sarath Chandra Mudigonda ",
-       position: "Head of Finance",
-       phone: "9884491080 ",
-       email: "sm261@snu.edu.in "
-     },
-     {
-       name: "Keval Sushruth",
-       position: "Sports Director",
-       phone: "8826243936",
-       email: "sk405@snu.edu.in"
-     },
-     {
-       name: "Kaustuv Acharyya ",
-       position: "Head of Public Relations",
-       phone: "9643427888",
-       email: "ka117@snu.edu.in"
-     },
-     {
-       name: "Abhinav Keshri",
-       position: "Cultural Director",
-       phone: "9835534434",
-       email: "ak825@snu.edu.in"
-     },
-     {
-       name: "Abhirami Reddy",
-       position: "Sports Director",
-       phone: "9515502260",
-       email: "ar197@snu.edu.in "
-     }
-   ]
+  contacts() {
+    this.people.map(
+      (data) => {
+        return (
+          <View>
+          <Text>{e.name}</Text>
+          <Text>
+            <Text>{e.position} </Text>
+          </Text>
+          <Text>
+            <Text>{e.phone} </Text>
+          </Text>
+          <Text>
+            <Text>{e.email} </Text>
+          </Text>
+        </View>
+        )
+      }
+    )
+  }
 
- 
+  render() {
+    // const contacts = this.people.map(e => (
+    //   <View>
+    //     <Text>{e.name}</Text>
+    //     <Text>
+    //       <Text>{e.position} </Text>
+    //     </Text>
+    //     <Text>
+    //       <Text>{e.phone} </Text>
+    //     </Text>
+    //     <Text>
+    //       <Text>{e.email} </Text>
+    //     </Text>
+    //   </View>
+    // ));
 
-   render() {
-   const contacts = this.people.map(e => 
-     <View
-     >
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headingText}>Contact Us</Text>
 
-       <Text>
-       {e.name}
-       </Text>
-       <Text>
-       
-         <Text>{e.position} </Text>
-       </Text>
-       <Text>
-       
-         <Text>{e.phone} </Text>
-       </Text>
-       <Text>
-         <Text >{e.email} </Text>
-       </Text>
-     
-   </View>	
-   )
+        <Text
+          style={styles.headingText}
+        >
+          {/* {this.contacts()} */}
+        </Text>
 
-
-       return (
-           
-   <Text>
-
-       <Text >Contact Us</Text>
-
-
-     <Text>
-       {contacts}
-     </Text>
-
-           <Text>
-                   Reach Us At
-                 </Text> 
-               <Text>
-                 breeze@snu.edu.in
-               </Text>
-
-         <Text>
-           Address
-         </Text> 
-         <Text >
-           Shiv Nadar University NH - 91, Tehsil Dadri Gautam Buddha Nagar Uttar Pradesh - 201314
-         </Text>
-
-
-   </Text>
-       )
-   }
+        <View style={styles.darkBG}>
+          <Text style={{...styles.primaryText, ...styles.upperText}}>Reach Us At</Text>
+          <Text style={styles.secondaryText}>breeze@snu.edu.in</Text>
+          
+          <Text style={{...styles.primaryText, ...styles.upperText, marginTop: 10}}>Address</Text>
+          <Text style={{...styles.secondaryText}}>
+            Shiv Nadar University NH - 91, Tehsil Dadri Gautam Buddha Nagar Uttar
+            Pradesh - 201315
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
-
 
 ContactScreen.navigationOptions = {
   header: null
@@ -151,7 +153,37 @@ ContactScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15,
-    backgroundColor: 'rgba(0,0,0,0)',
+    padding: 35,
+    paddingTop: 50,
+    backgroundColor: "rgba(0,0,0,0)"
   },
+  headingText: {
+    fontFamily: "just-fist",
+    fontSize: 50,
+    color: "#fff",
+    letterSpacing: 4,
+    textShadowColor: "#000000",
+    textShadowRadius: 10,
+    textAlign: "center",
+    textShadowOffset: {
+      width: 5,
+      height: 0
+    },
+  },
+  darkBG: {
+    backgroundColor: "rgba(25, 25, 25, 0.95)",
+    padding: 20,
+    borderRadius: 15
+  },
+  primaryText: {
+    color: Colors.gullyRed,
+    fontSize: 20
+  },
+  secondaryText: {
+    color: Colors.gullyOrange,
+    fontSize: 15
+  },
+  upperText: {
+    textTransform: "uppercase"
+  }
 });
